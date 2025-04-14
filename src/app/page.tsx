61dca86d0f2ea8c3b2634cf93ed0d8c1914eae9d
ajjixes <118143579+ajjixes@users.noreply.github.com>
2025-04-14 21:25:33 +0800

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+import { InteractiveGridPattern } from "@/components/magicui/interactive-grid-pattern";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen w-full flex flex-col items-center justify-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+
+    </div>
+  );
+}
